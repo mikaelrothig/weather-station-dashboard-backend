1e24c649b18e2b691525f233af9070f0330338e0
MikaelRothig <mrrothig@gmail.com>
2025-02-13 20:55:04 +0200

    Added required params

--- src/WindguruApi.ts
+++ src/WindguruApi.ts
@@ -1,9 +1,18 @@
 import axios, { AxiosResponse } from 'axios';
 
+interface WindguruParams {
+    q: string;
+    id_model: number;
+    rundef: string;
+    id_spot: number;
+    WGCACHEABLE: number;
+    cachefix: string;
+}
+
 class WindguruApi {
     private static readonly BASE_URL = 'https://www.windguru.net/int/iapi.php';
 
-    public static async fetchData(params: Record<string, string | number>): Promise<any> {
+    public static async fetchData(params: WindguruParams): Promise<any> {
         try {
             const response: AxiosResponse = await axios.get(this.BASE_URL, { params });
             return response.data;
