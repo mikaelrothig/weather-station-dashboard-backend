078cfd8d392d260ab6da447c2881afc909027ab5
MikaelRothig <mrrothig@gmail.com>
2025-02-15 00:08:06 +0200

    Added second route

--- src/server.ts
+++ src/server.ts
@@ -16,8 +16,8 @@ const generateRundef = () => {
     const now = new Date();
     const gmtMinus7 = new Date(now.getTime() - 7 * 60 * 60 * 1000);
     const year = gmtMinus7.getFullYear();
-    const month = String(gmtMinus7.getMonth() + 1).padStart(2, '0'); // Ensure 2-digit format
-    const day = String(gmtMinus7.getDate()).padStart(2, '0'); // Ensure 2-digit format
+    const month = String(gmtMinus7.getMonth() + 1).padStart(2, '0');
+    const day = String(gmtMinus7.getDate()).padStart(2, '0');
     const hour = gmtMinus7.getHours();
 
     let hourSuffix;
@@ -31,15 +31,15 @@ const generateRundef = () => {
         hourSuffix = "00";
     }
 
-    return `${year}${month}${day}${hourSuffix}x0x78x0x78`;
+    return `${year}${month}${day}${hourSuffix}`;
 };
 
-app.get('/api/windguru', async (req: Request, res: Response) => {
+app.get('/api/windguru/wrf-9km', async (req: Request, res: Response) => {
     try {
         const params = {
             q: req.query.q as string || 'forecast',
             id_model: Number(req.query.id_model) || 36,
-            rundef: req.query.rundef as string || generateRundef(),
+            rundef: req.query.rundef as string || generateRundef()+'x0x78x0x78',
             id_spot: Number(req.query.id_spot) || 208276,
             WGCACHEABLE: Number(req.query.WGCACHEABLE) || 21600,
             cachefix: req.query.cachefix as string || '-33.82x18.47x0',
@@ -50,6 +50,25 @@ app.get('/api/windguru', async (req: Request, res: Response) => {
     } catch (error) {
         console.error('Error fetching data:', error);
         res.status(500).json({ error: 'Failed to fetch Windguru data' });
+    }
+});
+
+app.get('/api/windguru/gfs-13km', async (req: Request, res: Response) => {
+    try {
+        const params = {
+            q: req.query.q as string || 'forecast',
+            id_model: Number(req.query.id_model) || 3,
+            rundef: req.query.rundef as string || generateRundef()+'x0x240x0x240-'+generateRundef()+'x243x384x243x384',
+            id_spot: Number(req.query.id_spot) || 208276,
+            WGCACHEABLE: Number(req.query.WGCACHEABLE) || 21600,
+            cachefix: req.query.cachefix as string || '-33.82x18.47x0',
+        };
+
+        const data = await WindguruApi.fetchData(params);
+        res.json(data);
+    } catch (error) {
+        console.error('Error fetching alternative data:', error);
+        res.status(500).json({ error: 'Failed to fetch alternative Windguru data' });
     }
 });
 
